dfaa54e709a8fd678ecd33f7ff7a6e79859c3712
ykhorizon <ykhorizon.light@gmail.com>
2017-10-22 12:18:11 +0800

    clean code

--- python_src/main.py
+++ python_src/main.py
@@ -1,112 +1,148 @@
-
 import numpy as np
 import helper as helper
-import datetime 
-import plot_main as pm
+import datetime
+import plot as myPloyLib
 from Controller.MIController import MIController
+from Controller.MIController import SimpleController
 from settings import ConfigHolder
-
+from Simulator.EnvSimulator import EnvSimulator
 
 
 if __name__ == "__main__":
 
-    # Configuration
+    project = 'TOMATO2'
+    envSimu = EnvSimulator(project)
+    envSimu.initRun()
+
     cfgHolder = ConfigHolder()
     config = cfgHolder.get()
-    prefixLIST = config['path_prefix']['AC_plugin_LIST']
-    prefixOUTP = config['path_prefix']['AC_plugin_OUTP']
-    prefixIRR = config['path_prefix']['AC_DATA']
 
-    # File name
-    name = 'TOMATO2'
-    PROExt = '.PRO'
-    dotPROName = name + PROExt
-
-    OUTExt = '.OUT'
-    dotOUTName = name + 'PROday' + OUTExt
-
-    # Initialization
-    # Copy a .PRO backup with .PRO.BACKUP
-    helper.copyDotPROFile(dotPROName)
-
-    # Store the end of simu-day to variable to control simulation loop
-    configList = helper.readDotPROFile(dotPROName)
-    # print( configList )
-
-    firstSimuDay = int(configList[1])
-    lastSimuDay = int(configList[2])
-    firstCropDay = int(configList[3])
-    lastCropDay = int(configList[4])
-    # print( 'firstSimuDay:{}'.format( firstSimuDay ) )
-    # print( 'lastSimuDay:{}'.format( lastSimuDay ) )
-
-    # Do First Simulation to generate whole moisture(VWC) variation
-    helper.cleanExampleDotIrrFile()
-    helper.executeAquaCropPlugin()
-    pathOUT = prefixOUTP + dotOUTName
-    dailyData = np.loadtxt(pathOUT, skiprows=4)
-
-    # PROday.OUT index from settings.py
-    WC1Idx = config['day_data_index']['wc1']
-    WC2Idx = config['day_data_index']['wc1'] + 1
-    WC3Idx = config['day_data_index']['wc1'] + 2
-    WC4Idx = config['day_data_index']['wc1'] + 3
-    WC5Idx = config['day_data_index']['wc1'] + 4
-    WC6Idx = config['day_data_index']['wc1'] + 5
-    WC7Idx = config['day_data_index']['wc1'] + 6
-    WC8Idx = config['day_data_index']['wc1'] + 7
-    zIdx = config['day_data_index']['zoot']
-    ETIdx = config['day_data_index']['ET']
-    rainIdx = config['day_data_index']['rain']
-
-    # Initialization miControllerontroller
-    miController = MIController(ref1=25.0, ref2=10.0)
-    miController.setK(1.5, 2.25, 0, 0)
-
-    
-    compartmentBoundary = config['compartmentBoundary']
-    
-    # Start Iterate simulation loop, simulation start from day1
-    # Predict how to irrigation at day2...
-    currentSimuDay = firstSimuDay
-    dailyDataPointer = 0
+    #
+    # Initialize Controller
+    #
+    depth = 0
+    ref = 20
+    originalRef = ref
     predictedIrri = 0
-    while(currentSimuDay < lastSimuDay):
+    soilProperityDict = {"sat": 50, "fc": 30, "pwp": 10}
+    soilProfile = {"compartmentNum": 10}
+    sensorIntepret = {"controlDepth": depth}
+    simpleCtl = SimpleController(
+        soilProperityDict, soilProfile, sensorIntepret)
 
-        ##
-        # Prompt Info.
-        ##
-        dayCount=currentSimuDay-firstSimuDay+1
-        print('==== Simu day: {} ==== \n'.format(dayCount))
+    # Initialize reference of all layer
+    refsArray = np.zeros(soilProfile['compartmentNum'])
+    refsArray[depth] = ref
+    simpleCtl.set_ref(refsArray)
 
-        dailyData = np.loadtxt(pathOUT, skiprows=4)
-        row = dailyData[dailyDataPointer]
-        wc1 = row[WC1Idx]
-        wc2 = row[WC2Idx]
-        wc3 = row[WC3Idx]
-        wc4 = row[WC4Idx]
-        wc5 = row[WC5Idx]
-        wc6 = row[WC6Idx]
-        wc7 = row[WC7Idx]
-        wc8 = row[WC8Idx]
-        rain = row[rainIdx]
-        rDepth = row[zIdx]
-        ET = row[ETIdx]
-        ##
-        # Find floating EWC( Efficient Wetting Zone )
-        ##
-        firstQuarter = rDepth * 0.5
-        closestCompartment = helper.calEWZbyCompartment(
-            compartmentBoundary, firstQuarter)
+    # intialize PID k in all layer, Set Kp,Ki,Kd cefficient
+    kArray = np.zeros((3, soilProfile['compartmentNum']))
+    kArrayIndex = simpleCtl.get_kArray_index()
+    ku = 4
+    tu = 0.7
+    for i in range(0, soilProfile['compartmentNum']):
+        kArray[kArrayIndex['p'], i] = 0.45 * ku
+        kArray[kArrayIndex['i'], i] = 0
+        kArray[kArrayIndex['i'], i] = tu / 2
+        kArray[kArrayIndex['d'], i] = 0
+        # kArray[kArrayIndex['d'],depth]=tu/8
+        simpleCtl.set_k(kArray)
+    print("PID K")
+    print(kArray)
 
-        EWZGrowIdx = int(closestCompartment * 10 - 1)
-        EWZBottomWaterContent = row[WC1Idx + EWZGrowIdx]
-        
+    # Set windup
+    simpleCtl.set_windup(50)
+
+    #
+    # end of intialize controller
+    #
+
+    # declare controller output
+    statusList = []
+    statusList.append("day,sensor0depth,sensor1depth\n")
+    sensor0TrackList = []
+    sensor1TrackList = []
+
+    #
+    # simulation main loop the whole season
+    #
+    while not envSimu.isFinish():
+
+        print("\nDay Count: {}".format(envSimu.currentDayCount))
+        envSimu.loadResult()
+
+        # print("WCs:")
+        # print(envSimu.getCurrentDayWCs())
+        WCs = envSimu.getCurrentDayWCs()
+
+        # irrHistory = envSimu.getIrrigationHistory()
+        # print(WCs[0])
 
         """
             Methodology Part:
             If moisture condition triggers irrigation event, controller to generate irrigation amount
         """
+
+        # # IrriHistory
+
+        # if envSimu.currentDayCount == 0:
+        #     yesterIrrStr = "No yesterday"
+        # else:
+        #     yesterdayIdx = envSimu.currentDayCount - 1
+        #     yesterIrrStr = str(irrHistory[yesterdayIdx])
+
+        ##
+        # Find floating EWC( Efficient Wetting Zone )
+        ##
+        halfRootDepth = envSimu.getCurrentRootDepth() * 0.1
+        compartmentBoundary = config['compartmentBoundary']
+        closestCompartment = helper.calEWZbyCompartment(
+            compartmentBoundary, halfRootDepth)
+        sensor0 = int(closestCompartment * 10 - 1)
+
+        print("moving sensor0: {}".format(sensor0))
+        simpleCtl.set_controlDepth(sensor0)
+        sensor0TrackList.append(sensor0)
+
+        ##
+        # *self-adapt with sensor0 and sensor1
+        ##
+        sensor1 = sensor0 + 3
+        sensor1TrackList.append(sensor1)
+
+        # update ref0
+        # if WCs[sensor1] > 20 and refsArray[sensor0] > 28:
+        #     ref = ref - 1
+        refsArray[sensor0] = ref
+
+        # # global static ref0
+        # staticRef0 = [35, 32, 31, 30, 29]
+        # staticRef0 = [35, 33, 32, 31, 30]
+        # refsArray[sensor0] = staticRef0[sensor0]
+        # ref = staticRef0[sensor0]
+        # Display adaptive ref of sensor0
+        print("sensor0 Ref: {}".format(ref))
+        simpleCtl.set_ref(refsArray)
+
+        # notate current sensor0 and sensor1 in soil layer
+        print("WCs:")
+        strWCs = list(map(str, envSimu.getCurrentDayWCs()))
+        strWCs[sensor0] = "*" + strWCs[sensor0]
+        # strWCs[sensor1] = "$" + strWCs[sensor1]
+        print(strWCs)
+
+        print("Refs:")
+        strRefsArray = list(map(str, refsArray))
+        strRefsArray[sensor0] = "*" + strRefsArray[sensor0]
+        # strRefsArray[sensor1] = "$" + strRefsArray[sensor1]
+        print(strRefsArray)
+
+        print("Error:")
+        strErrors = list(map(str, simpleCtl.get_error(WCs)))
+        strErrors[sensor0] = "*" + strErrors[sensor0]
+        # strErrors[sensor1] = "$" + strErrors[sensor1]
+        print(strErrors)
+
         
         ##
         # -- ET-base Irrigation --
@@ -115,47 +151,38 @@ if __name__ == "__main__":
         # predictedIrri = helper.ETbasedIrrigation(ET, ET_k)
 
         ##
-        # -- SI-controller --: shallow point = 0.05m, 0.15m, 0.25m(wc1, wc2, wc3)
+        # -- MI-controller --: shallow point = 0.05m, 0.15m, 0.25m(wc1, wc2, wc3)
         ##
-        predictedIrri = helper.mockControllerBySI(wc2, 25)
+        # print("error:")
+        # print(simpleCtl.get_error(WCs))
+        predictedIrri = simpleCtl.get_output(WCs)
+        controllerStatus = simpleCtl.get_status(WCs)
 
-        ## -- MI-controller without floating EWC --
-        ## fixed shallow and deep
-
-        ## -- MI-controller with floating EWC --
-        ## shallow point = 0.05m, 0.15m, 0.25m(wc1, wc2, wc3)
-        # miController.update(shallow_wc, EWZBottomWaterContent, rain, dailyDataPointer+1)
-        # predictedIrri = miController.get_output()
-        shallow_wc = wc1
-        ## -- MI-controller with EWC and adaptive --
-
-
-        # state = miController.get_state(dayCount)
-        # print(' state: {}\n'.format(state))
-        # Filter out negative irrigation amount
+        statusList.append("{},{},{}".format(envSimu.getCurrentDayCount(), sensor0, sensor1))
         if predictedIrri > 0:
 
-            # Append Irrigatio Event into Example.Irr
-            # tomorrow = today+1
-            irriDay = dayCount + 1
+            irriDay = envSimu.currentDayCount + 1
+            print("IrriDay: {}".format(irriDay))
+            print("To Irrigate: {}".format(predictedIrri))
             helper.appendDotIRR('Example.Irr', irriDay, predictedIrri)
+            envSimu.runOnce()
 
-            # Prompt Info.
-            print('wc_shallow: {}, EWZ_bottom: {}, zDepth: {}, closest compartment: {} \n'.format(
-                shallow_wc, EWZBottomWaterContent, firstQuarter, closestCompartment))
-            e1 = miController.get_error1()
-            e2 = miController.get_error2()
-            print("e1: {}, e2: {}\n".format(e1, e2))
-            print("Tomorrow( {} ) Irri: {:.2f} \n\n".format(irriDay, predictedIrri))
+        envSimu.increateTimeStep()
 
-            # Re-simulation the whole procces () to evaluate add new irrigation event
-            helper.executeAquaCropPlugin()
-        else:
-            predictedIrri = 0
+    # End the while loop
 
+    # Write out the result
+    sharedInfoPath = r"D:\yk_research\AquaCrop-Irrigation-Design\output\moving_control_point\shared_info.json"
+    sharedInfo = helper.readSharedInfo(sharedInfoPath)
 
-        currentSimuDay = currentSimuDay + 1
-        dailyDataPointer = dailyDataPointer + 1
+    simulationName = "depth{}_ref{}_moving_{}".format(
+        depth, originalRef, sharedInfo["current_unique_id"])
+    envSimu.writeOutResult(project, simulationName)
+    helper.increaseIdSharedInfo(sharedInfoPath)
 
-pm.plot_main()
+    # create single experiment notes
+    # expJSONdestPath = "D:\yk_research\AquaCrop-Irrigation-Design\output\moving_control_point\{}.json".format(simulationName))
+    # helper.createExpJSON(sharedInfoPath, expJSONdestPath)
 
+    # Plot WC color diagram
+    myPloyLib.plot_img_test(sensor0TrackList, sensor1TrackList)
